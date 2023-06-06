ffb60e040f97c0600e568ab016b2d55c78f290e3
--replace-all <purvadjoshi@gmail.com>
2023-06-06 22:48:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import './App.css';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Feature,
+  WhatGPT3,
+  Header,
+  Features,
+} from "./containers";
+import { Brand, CTA, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+        <div className="gradient__bg">
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
+  );
+};
+
+export default App;
